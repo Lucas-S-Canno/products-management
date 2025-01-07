e39db150b0b62c2f72433a2249bb13b0c6c7764d
LUCAS SACHINI CANNO <lucas.s.canno@hotmail.com>
2025-01-07 00:12:00 -0300

    feat: improving product logging

--- src/main/java/com/productsmanagement/productsmanagement/component/RabbitMQListener.java
+++ src/main/java/com/productsmanagement/productsmanagement/component/RabbitMQListener.java
@@ -1,16 +1,28 @@
 package com.productsmanagement.productsmanagement.component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.productsmanagement.productsmanagement.config.RabbitMQConfig;
 import com.productsmanagement.productsmanagement.dto.ProductDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitMQListener {
 
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQListener.class);
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
     public void listen(ProductDTO product) {
-        System.out.println("Mensagem recebida: " + product);
+        try {
+            String productJson = objectMapper.writeValueAsString(product);
+            logger.info("Mensagem recebida: {}", productJson);
+        } catch (JsonProcessingException e) {
+            logger.error("Erro ao converter ProductDTO para JSON", e);
+        }
     }
 
 }
