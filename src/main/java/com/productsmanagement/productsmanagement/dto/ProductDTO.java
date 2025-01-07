b5a4075500271affad3fb481c76aad04af12b213
LUCAS SACHINI CANNO <lucas.s.canno@hotmail.com>
2025-01-06 23:32:21 -0300

    feat: implementing serializable

--- src/main/java/com/productsmanagement/productsmanagement/dto/ProductDTO.java
+++ src/main/java/com/productsmanagement/productsmanagement/dto/ProductDTO.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductDTO {
+public class ProductDTO implements Serializable {
     private Long id;
     private String name;
     private String description;
