972db4d5ad21a88f3ab9505a8f6659a72f5657fd
LUCAS SACHINI CANNO <lucas.s.canno@hotmail.com>
2025-01-09 00:19:40 -0300

    refactor: change imports

--- src/main/java/com/productsmanagement/productsmanagement/dto/ProductDTO.java
+++ src/main/java/com/productsmanagement/productsmanagement/dto/ProductDTO.java
@@ -1,9 +1,6 @@
 package com.productsmanagement.productsmanagement.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,9 +17,14 @@ public class ProductDTO implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
+    @Column(name = "price")
     private Double price;
+    @Column(name = "quantity")
     private Integer quantity;
+    @Column(name = "category")
     private String category;
 }
